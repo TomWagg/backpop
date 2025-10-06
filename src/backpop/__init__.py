056eef8857b6e3d21dedd050d9c7839f54eb320d
Tom Wagg <tomjwagg@gmail.com>
2025-10-06 13:46:25 -0400

    update init

--- src/backpop/__init__.py
+++ src/backpop/__init__.py
@@ -1,3 +1,4 @@
-from . import main, phase, consts, files
+from . import main, posteriors, phase, consts, files
 from ._version import __version__
 from .main import BackPop
+from .posteriors import BackPopPosteriors
